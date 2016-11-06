72913cfbe1cb93f19f0ec81f66a7163c99d3a9e5
dominikus1993 <dominikus1910@gmail.com>
2016-11-06 20:45:06 +0100

    add Tweet Controller and first method

---
+++ src/Server/Controllers/TweetController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Core.Models;
+using Core.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class TweetController : Controller
+    {
+        private readonly ITweetService _tweetService;
+
+        public TweetController(ITweetService tweetService)
+        {
+            _tweetService = tweetService;
+        }
+
+
+        [HttpGet("{key}")]
+        public Result<IEnumerable<Tweet>> Get(string key)
+        {
+            return _tweetService.GetTweetByKey(key);
+        }
+    }
+}
