3f4245314d47522a067e037813697e584c9bc179
dominikus1993 <dominikus1910@gmail.com>
2016-11-07 21:03:25 +0100

    add afinn.json file

---
+++ src/Server/Utils/File.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Server.Utils
+{
+    public class File
+    {
+        public Dictionary<string, int> GetAfinnJsonFile()
+        {
+
+        }
+    }
+}
