3032eaa607700ed5119a05b865fb419ea97953f0
Dominik Kotecki <dominikus1910@gmail.com>
2017-01-03 20:49:16 +0100

    add hangfire

---
+++ src/Server/Hangfire/AllowAnyAuthorizationFilter.cs
@@ -0,0 +1,12 @@
+﻿using Hangfire.Dashboard;
+
+namespace Server.Hangfire
+{
+    public class AllowAnyAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            return true;
+        }
+    }
+}
